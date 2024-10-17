6e2ee363a1c64781b764f7463bd9999f8c896be3
DANIA CORTES DOMINGUEZ <daniacortes324@gmail.com>
2024-10-17 10:22:52 -0600

    Se agrego un mensjae

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if (!empty($_SESSION['activa'])) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Inicio de Sesión</title>
+  <title>Inicio de Autentificación</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
